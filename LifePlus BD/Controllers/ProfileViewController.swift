a26e14b2246a742486083688ce825d88d5a4c926
Abdullah Al Mahfug <54243174+AbdalMahfuj@users.noreply.github.com>
2023-05-06 16:53:26 +0600

    border corner design of uiviews of profileVC

--- LifePlus BD/Controllers/ProfileViewController.swift
+++ LifePlus BD/Controllers/ProfileViewController.swift
@@ -16,12 +16,51 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var phoneLabel: UILabel!
     
+    @IBOutlet weak var userNameView: UIView!
+    @IBOutlet weak var nameView: UIView!
+    @IBOutlet weak var phoneView: UIView!
+    
+    @IBOutlet weak var innerNameView: UIView!
+    @IBOutlet weak var InnerPhoneView: UIView!
+    @IBOutlet weak var InnerUsernameView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         nameLabel.text = "Name: \(user?.name ?? "Name not found")"
         usernameLabel.text = "Username: \(user?.userName ?? "Username not found")"
         phoneLabel.text = "Phone: \(user?.phone ?? "Phone not found")"
+        
+        nameView.layer.borderWidth = 1
+        nameView.layer.borderColor = UIColor.systemGreen.cgColor
+        nameView.layer.cornerRadius = 5
+        
+        userNameView.layer.borderWidth = 1
+        userNameView.layer.borderColor = UIColor.systemGreen.cgColor
+        userNameView.layer.cornerRadius = 5
+        
+        phoneView.layer.borderWidth = 1
+        phoneView.layer.borderColor = UIColor.systemGreen.cgColor
+        phoneView.layer.cornerRadius = 5
+        
+        innerNameView.layer.borderWidth = 1
+        innerNameView.layer.borderColor = UIColor.systemGray2.cgColor
+        innerNameView.layer.cornerRadius = 10
+        
+        InnerPhoneView.layer.borderWidth = 1
+        InnerPhoneView.layer.borderColor = UIColor.systemGray2.cgColor
+        InnerPhoneView.layer.cornerRadius = 10
+        
+        InnerUsernameView.layer.borderWidth = 1
+        InnerUsernameView.layer.borderColor = UIColor.systemGray2.cgColor
+        InnerUsernameView.layer.cornerRadius = 10
+        
+        
+        
+        
+        
+        
     }
     
 
